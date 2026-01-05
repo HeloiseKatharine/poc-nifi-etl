7c88db287ba396477ee2e0961c9c44598aa0f352
rodrigodiasnoronha <rodrigodnoronha@gmail.com>
2026-01-05 12:24:38 -0300

    chore: melhorando doc projeto

--- scripts/testar-conexao-sftp.py
+++ scripts/testar-conexao-sftp.py
@@ -1,26 +1,20 @@
 import paramiko
 
-# Configurações
 hostname = 'localhost'
 port = 2222
 username = 'caixagis'
 password = 'caixagis123'
 
-# Conectar
 transport = paramiko.Transport((hostname, port))
 transport.connect(username=username, password=password)
 sftp = paramiko.SFTPClient.from_transport(transport)
 
-# Listar arquivos
-print("Arquivos em /upload:")
-print(sftp.listdir('/upload'))
+#print("Arquivos em /upload:")
+# print(sftp.listdir('/upload'))
 
-# Upload de arquivo
-# sftp.put('local_file.txt', '/upload/remote_file.txt')
 
 # Download de arquivo
 # sftp.get('/download/remote_file.txt', 'local_file.txt')
 
-# Fechar conexão
 sftp.close()
 transport.close()
